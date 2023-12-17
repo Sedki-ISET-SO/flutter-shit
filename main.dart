0bd17d0d8e48ce99359c972aacaae22bd0489b81
U2Vka2kgQmVuIFppZCBNRFczLjEgSVNFVCBTT1VTU0U= <74719965+Sedki-ISET-SO@users.noreply.github.com>
2023-12-17 12:36:37 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -13,7 +13,149 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         // home: RandomIconScreen(),
-        home: ItemForm());
+        home: CalculatorApp());
+  }
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: CalculatorPage(),
+    );
+  }
+}
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "÷" ||
+        buttonText == "×") {
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already contains a decimal");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "÷") {
+        _output = (num1 / num2).toString();
+      }
+      if (operand == "×") {
+        _output = (num1 * num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    print(_output);
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget calculatorButton(
+      String buttonText, Color buttonColor, Color textColor) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        primary: textColor,
+        backgroundColor: buttonColor,
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(20.0),
+      ),
+      onPressed: () => buttonPressed(buttonText),
+      child: Text(buttonText, style: TextStyle(fontSize: 30)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Calculator')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.orange.shade200,
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                output,
+                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.count(
+              crossAxisCount: 4,
+              children: <Widget>[
+                calculatorButton("C", Colors.grey, Colors.black),
+                calculatorButton("⌫", Colors.grey, Colors.black),
+                calculatorButton("÷", Colors.orange, Colors.white),
+                calculatorButton("×", Colors.orange, Colors.white),
+                calculatorButton("7", Colors.white, Colors.black),
+                calculatorButton("8", Colors.white, Colors.black),
+                calculatorButton("9", Colors.white, Colors.black),
+                calculatorButton("−", Colors.orange, Colors.white),
+                calculatorButton("4", Colors.white, Colors.black),
+                calculatorButton("5", Colors.white, Colors.black),
+                calculatorButton("6", Colors.white, Colors.black),
+                calculatorButton("+", Colors.orange, Colors.white),
+                calculatorButton("1", Colors.white, Colors.black),
+                calculatorButton("2", Colors.white, Colors.black),
+                calculatorButton("3", Colors.white, Colors.black),
+                calculatorButton("=", Colors.orange, Colors.white),
+                calculatorButton("0", Colors.white, Colors.black),
+                calculatorButton(".", Colors.grey, Colors.black),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
