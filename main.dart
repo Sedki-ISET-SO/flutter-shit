7d9cd2d5ab04211d8f66ba11202cd16c4ce5ecf9
U2Vka2kgQmVuIFppZCBNRFczLjEgSVNFVCBTT1VTU0U= <74719965+Sedki-ISET-SO@users.noreply.github.com>
2023-12-17 12:30:41 +0100

    Add files via upload

--- main.dart
+++ main.dart
@@ -13,9 +13,140 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
         // home: RandomIconScreen(),
-        home: ItemForm());
+        home: CalculatorApp());
   }
 }
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: CalculatorPage(),
+    );
+  }
+}
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String output = "12345679";
+
+  // Add your logic for button presses here
+
+  Widget calculatorButton(String buttonText, Color buttonColor, Color textColor) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        primary: textColor,
+        backgroundColor: buttonColor,
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(20.0),
+      ),
+      onPressed: () {
+        // Handle button press
+      },
+      child: Text(buttonText, style: TextStyle(fontSize: 30)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Calculator')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.orange.shade200,
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
+              child: Text(
+                output,
+                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.count(
+              crossAxisCount: 4,
+              children: <Widget>[
+                calculatorButton("C", Colors.grey, Colors.black),
+                calculatorButton("⌫", Colors.grey, Colors.black),
+                calculatorButton("÷", Colors.orange, Colors.white),
+                calculatorButton("×", Colors.orange, Colors.white),
+                calculatorButton("7", Colors.white, Colors.black),
+                calculatorButton("8", Colors.white, Colors.black),
+                calculatorButton("9", Colors.white, Colors.black),
+                calculatorButton("−", Colors.orange, Colors.white),
+                calculatorButton("4", Colors.white, Colors.black),
+                calculatorButton("5", Colors.white, Colors.black),
+                calculatorButton("6", Colors.white, Colors.black),
+                calculatorButton("+", Colors.orange, Colors.white),
+                calculatorButton("1", Colors.white, Colors.black),
+                calculatorButton("2", Colors.white, Colors.black),
+                calculatorButton("3", Colors.white, Colors.black),
+                calculatorButton("=", Colors.orange, Colors.white),
+                calculatorButton("0", Colors.white, Colors.black),
+                calculatorButton(".", Colors.grey, Colors.black),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+  // Add your logic for button presses here
+
+  Widget calculatorButton(String buttonText) {
+    return ElevatedButton(
+     
+      onPressed: () {
+        // Handle button press
+      },
+      child: Text(buttonText, style: TextStyle(fontSize: 30)),
+      
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Calculator')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.bottomRight
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+              itemCount: 16, // total number of buttons
+              itemBuilder: (BuildContext context, int index) {
+                // return your button here
+                return calculatorButton("1"); // Replace with actual button text
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 
 class RandomIconScreen extends StatefulWidget {
   const RandomIconScreen({super.key});
